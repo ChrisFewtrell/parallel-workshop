42b62f948ff3a1c3d37fa4aa156e5d8654950632
Peter Chase <github@peterchase.me>
2015-09-17 19:59:37 +0100

    Add a solution - one way of doing Ex02

---
+++ ParallelWorkshop/Ex02LineCount/SolutionWithLazy/CountedTextFile.cs
@@ -0,0 +1,43 @@
+﻿using FileData.Reading;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System;
+
+namespace ParallelWorkshop.Ex02LineCount.SolutionWithLazy
+{
+    public class CountedTextFile : ICountedTextFile
+    {
+        private readonly ITextFile textFile;
+        private readonly Lazy<int> lazyCount;
+
+        private int numberOfFileReads;
+
+        public CountedTextFile(ITextFile textFile)
+        {
+            this.textFile = textFile;
+            lazyCount = new Lazy<int>(CountLines, LazyThreadSafetyMode.ExecutionAndPublication);
+        }
+
+        public IEnumerable<string> ReadLines()
+        {
+            return textFile.ReadLines();
+        }
+
+        public int LineCount
+        {
+            get { return lazyCount.Value; }
+        }
+
+        public int NumberOfCallsToCountLines
+        {
+            get { return numberOfFileReads; }
+        }
+
+        private int CountLines()
+        {
+            Interlocked.Increment(ref numberOfFileReads);
+            return ReadLines().Count();
+        }
+    }
+}
