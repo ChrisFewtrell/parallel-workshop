62b96949bcc1ae0caf5176bc9eb417263efe34fd
Peter Chase <peter.chase@grantadesign.com>
2015-09-18 16:08:13 +0100

    Add Ex05ReaderWriter, with no solution yet

---
+++ ParallelWorkshop/Ex09ReaderWriter/MultiFileCharacterCounter.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using Lurchsoft.FileData;
+using Lurchsoft.FileData.Characters;
+
+namespace Lurchsoft.ParallelWorkshop.Ex09ReaderWriter
+{
+    public class MultiFileCharacterCounter : IMultiFileCharacterCounter
+    {
+        private readonly CharacterTotaliser totaliser = new CharacterTotaliser();
+
+        public IReadOnlyDictionary<char, int> GetCharCounts()
+        {
+            return totaliser.GetCharCounts();
+        }
+
+        public void Add(ITextFile textFile)
+        {
+            foreach (string line in textFile.ReadLines())
+            {
+                totaliser.Add(line);
+            }
+        }
+    }
+}
---
+++ ParallelWorkshopTests/Ex09ReaderWriter/MultiFileCharacterCounterTests.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Lurchsoft.FileData;
+using Lurchsoft.ParallelWorkshop.Ex09ReaderWriter;
+using NUnit.Framework;
+
+namespace Lurchsoft.ParallelWorkshopTests.Ex09ReaderWriter
+{
+    [TestFixture]
+    public class MultiFileCharacterCounterTests
+    {
+        [Test]
+        public void GetCharCounts_ShouldReturnDoubledValues_WhenSameFileAddedSecondTime()
+        {
+            var counter = new MultiFileCharacterCounter();
+            counter.Add(EmbeddedFiles.Large);
+            var firstResults = counter.GetCharCounts();
+            counter.Add(EmbeddedFiles.Large);
+            Assert.That(counter.GetCharCounts().All(secondResult => IsDoubled(secondResult, firstResults)));
+        }
+
+        [Test]
+        public void GetCharCounts_ShouldReturnValue_WhenCalledWhileAddsAreOccurringOnOtherThreads()
+        {
+            const int NumAdds = 1000, NumGets = 10000;
+            var counter = new MultiFileCharacterCounter();
+            Task[] addTasks = Enumerable.Range(0, NumAdds).Select(i => Task.Factory.StartNew(() => counter.Add(EmbeddedFiles.Medium))).ToArray();
+            var results = Enumerable.Range(0, NumGets).Select(i => counter.GetCharCounts()).ToList();
+            Assert.That(results.All(d => d != null));
+            Task.WaitAll(addTasks);
+        }
+
+        private static bool IsDoubled(KeyValuePair<char, int> secondResult, IReadOnlyDictionary<char, int> firstResults)
+        {
+            return secondResult.Value == firstResults[secondResult.Key] * 2;
+        }
+    }
+}
