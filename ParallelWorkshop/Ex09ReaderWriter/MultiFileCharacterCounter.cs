7a670288cf6c0c50c495d40a0523796834ec3a20
Peter Chase <peter.chase@grantadesign.com>
2015-09-18 16:16:35 +0100

    Add a possible solution for Ex09ReaderWriter

--- ParallelWorkshop/Ex09ReaderWriter/MultiFileCharacterCounter.cs
+++ ParallelWorkshop/Ex09ReaderWriter/MultiFileCharacterCounter.cs
@@ -10,6 +10,12 @@ namespace Lurchsoft.ParallelWorkshop.Ex09ReaderWriter
 
         public IReadOnlyDictionary<char, int> GetCharCounts()
         {
+            // This will throw if someone adds a line to the totaliser while it's executing.
+            // We need to make it thread-safe and we want to allow multiple simultaneous reads.
+            //
+            // Try at least the following approaches: -
+            // - ReaderWriterLock
+            // - ReaderWriterLockSlim
             return totaliser.GetCharCounts();
         }
 
---
+++ ParallelWorkshop/Ex09ReaderWriter/PossibleSolution/ReaderWriterMultiFileCharacterCounter.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using System.Threading;
+using Lurchsoft.FileData;
+using Lurchsoft.FileData.Characters;
+
+namespace Lurchsoft.ParallelWorkshop.Ex09ReaderWriter.PossibleSolution
+{
+    public class ReaderWriterMultiFileCharacterCounter : IMultiFileCharacterCounter
+    {
+        private readonly ReaderWriterLockSlim readerWriterLock = new ReaderWriterLockSlim(LockRecursionPolicy.NoRecursion);
+        private readonly CharacterTotaliser totaliser = new CharacterTotaliser();
+
+        public IReadOnlyDictionary<char, int> GetCharCounts()
+        {
+            readerWriterLock.EnterReadLock();
+            try
+            {
+                return totaliser.GetCharCounts();
+            }
+            finally
+            {
+                readerWriterLock.ExitReadLock();
+            }
+        }
+
+        public void Add(ITextFile textFile)
+        {
+            // We could possibly put the lock around just Add(), but would then incur a lot of lock-entry/exit
+            // time. It could sometimes be better, but this way probably wins most times.
+            readerWriterLock.EnterWriteLock();
+            try
+            {
+                foreach (string line in textFile.ReadLines())
+                {
+                    totaliser.Add(line);
+                }
+            }
+            finally
+            {
+                readerWriterLock.ExitWriteLock();
+            }
+        }
+    }
+}
