11094788d6640835d01ed05871f93861fdd6ce40
Peter Chase <peter.chase@grantadesign.com>
2015-09-18 13:55:14 +0100

    A solution for Ex03Cache

---
+++ ParallelWorkshop/Ex03Cache/SolutionWithConcurrentCollection/CachingFileCharacterCounter.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Concurrent;
+using FileData;
+using FileData.Characters;
+
+namespace ParallelWorkshop.Ex03Cache.SolutionWithConcurrentCollection
+{
+    public class CachingFileCharacterCounter : IFileCharacterCounter
+    {
+        private readonly ConcurrentDictionary<ITextFile, ICharacterCounter> cache = new ConcurrentDictionary<ITextFile, ICharacterCounter>();
+
+        public int NumberOfCallsToComputeCharCounts { get; private set; }
+
+        public ICharacterCounter GetCharCounts(ITextFile textFile)
+        {
+            // Concise, huh? It's almost as if ConcurrentDictionary was designed for this...
+            return cache.GetOrAdd(textFile, ComputeCharCounts);
+        }
+
+        private ICharacterCounter ComputeCharCounts(ITextFile textFile)
+        {
+            ++NumberOfCallsToComputeCharCounts;
+            var counter = new CharacterTotaliser();
+            foreach (string line in textFile.ReadLines())
+            {
+                counter.Add(line);
+            }
+
+            return counter;
+        }
+    }
+}
