b6133c3858eccd57d063e5f8ce9127267fd284e7
Peter Chase <peter.chase@grantadesign.com>
2015-09-22 10:09:39 +0100

    Add ExecutionAndPublicationWithLocklazy

---
+++ ParallelWorkshop/Ex07DiyLazy/PossibleSolution/ExecutionAndPublicationWithLockLazy.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Threading;
+
+namespace Lurchsoft.ParallelWorkshop.Ex07DiyLazy.PossibleSolution
+{
+    public class ExecutionAndPublicationWithLockLazy<T> : ILazy<T> where T : class
+    {
+        private readonly Func<T> evaluator;
+        private readonly object sync = new object();
+
+        private T value;
+
+        public ExecutionAndPublicationWithLockLazy(Func<T> evaluator)
+        {
+            this.evaluator = evaluator;
+        }
+
+        public T Value
+        {
+            get
+            {
+                // Simple and easy to understand. But every read requires acquisition of the lock - not very fast.
+                lock (sync)
+                {
+                    return value = value ?? evaluator();
+                }
+            }
+        }
+
+        public LazyThreadSafetyMode Mode
+        {
+            get
+            {
+                return LazyThreadSafetyMode.ExecutionAndPublication;
+            }
+        }
+    }
+}
