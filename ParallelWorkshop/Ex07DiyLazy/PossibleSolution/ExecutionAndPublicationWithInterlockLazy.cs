cee23477c37f046969cfb9923c6cf902cf681341
Peter Chase <github@peterchase.me>
2015-09-22 21:30:08 +0100

    Add ExecutionAndPublicationWithInterlockLazy solution

---
+++ ParallelWorkshop/Ex07DiyLazy/PossibleSolution/ExecutionAndPublicationWithInterlockLazy.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Threading;
+
+namespace Lurchsoft.ParallelWorkshop.Ex07DiyLazy.PossibleSolution
+{
+    public class ExecutionAndPublicationWithInterlockLazy<T> : ILazy<T> where T : class
+    {
+        private readonly Func<T> evaluator;
+
+        private T value;
+        private int state;
+
+        public ExecutionAndPublicationWithInterlockLazy(Func<T> evaluator)
+        {
+            this.evaluator = evaluator;
+        }
+
+        public T Value
+        {
+            get
+            {
+                while (true)
+                { 
+                    int prevState = Interlocked.CompareExchange(ref state, 1, 0);
+                    switch (prevState)
+                    {
+                        case 0:
+                            T newValue = value = evaluator();
+                            Volatile.Write(ref state, 2);
+                            return newValue;
+
+                        case 1:
+                            Thread.Yield();
+                            break;
+
+                        case 2:
+                            return value;
+                    }
+                }
+            }
+        }
+
+        public LazyThreadSafetyMode Mode
+        {
+            get
+            {
+                return LazyThreadSafetyMode.ExecutionAndPublication;
+            }
+        }
+    }
+}
