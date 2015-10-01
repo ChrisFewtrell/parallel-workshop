7464d9e6f36ee7c0538b206c614c6e3d4040be09
Peter Chase <peter.chase@grantadesign.com>
2015-10-01 16:36:10 +0100

    xmldoc explains the exercise

--- ParallelWorkshop/Ex10WaitHalfWay/LimitedModeCharacterCounter.cs
+++ ParallelWorkshop/Ex10WaitHalfWay/LimitedModeCharacterCounter.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using Lurchsoft.FileData;
 using Lurchsoft.FileData.Characters;
 
@@ -10,6 +11,14 @@ namespace Lurchsoft.ParallelWorkshop.Ex10WaitHalfWay
     /// A character counter that simulates a situation where information is coming from a system that can only
     /// be either reading files or counting characters. If one thread tries to read while another counts, it
     /// will fail. This limitation is simulated by <see cref="SystemMode"/>.
+    /// <para>
+    /// The exercise is to enable concurrent use of this class, while still respecting <see cref="SystemMode"/> limitations.
+    /// Use of the <see cref="Barrier"/> class is suggested.
+    /// </para>
+    /// <para>
+    /// Once you've had success, see the test class's comment about using <see cref="ParallelEnumerable.AsParallel"/> and
+    /// see if you can work out what goes wrong when you use it. Pretty subtle stuff!
+    /// </para>
     /// </summary>
     public class LimitedModeCharacterCounter : ICharacterCounter
     {
--- ParallelWorkshopTests/Ex10WaitHalfWay/LimitedModeCharacterCounterTests.cs
+++ ParallelWorkshopTests/Ex10WaitHalfWay/LimitedModeCharacterCounterTests.cs
@@ -1,6 +1,5 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
-using System.Threading;
 using System.Threading.Tasks;
 using Lurchsoft.FileData;
 using Lurchsoft.ParallelWorkshop.Ex10WaitHalfWay;
